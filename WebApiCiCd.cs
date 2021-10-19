3993e274b969d3d724578225bad8e73cab0ee368
WarriorsSami <sbarbutdica@gmail.com>
2021-10-19 17:55:12 +0300

    feat(startup): create base project

---
+++ WebApiCiCd.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WebApiCiCd
+{
+    public class WebApiCiCd
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
